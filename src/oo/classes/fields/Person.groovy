57018266faeddbe06ab9c22e522f91164ba7f055
John Lewis <john.lewis@digital.hmrc.gov.uk>
2018-01-15 11:59:24 +0000

    Improved description

--- src/oo/classes/fields/Person.groovy
+++ src/oo/classes/fields/Person.groovy
@@ -1,26 +1,39 @@
 package oo.classes.fields
 
-/*
-    When a class is declared with a field that has an associated access modifier it is up to the developer to provide
-    the related getter and setter methods. These are know as fields.
-
-    However if no access modifier is provided, a private field is created with public getter and setters
-
- */
-
 class Person {
 
+    /*
+        When a class is declared with a field that has the access modifier in place it is referred to as a field.
+        Groovy will provide methods to access this in the form of person.name
+     */
     private String name
-    Integer age
+
+    /*
+        If is possible to omit the type of a field though this is considered bad practise unless you really need to take
+        advantage of duck typing
+     */
+    private weight
+
+    /*
+        When the access modifier is omitted the field becomes a property. Groovy will provide the private backing
+        adding a get and set method
+     */
+    Integer age  // property
+
+
 
     static void main(String[] args){
         def person1 = new Person()
 
-        // a setProperty method can be used
-        person1.setProperty('name','John')
-        assert person1.getProperty('name') == 'John'
+        person1.name = 'John'
+        assert person1.name == 'John'
+
+        person1.weight = 76.2
+        assert person1.weight == 76.2
 
         person1.setAge(36)
         assert person1.getAge() == 36
+
+
     }
 }
